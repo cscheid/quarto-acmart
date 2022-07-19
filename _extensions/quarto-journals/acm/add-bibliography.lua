78338284d6fd0fd751108e1c548efdf2a8e82e1b
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-07-19 14:33:31 -0700

    v0.0.1

--- _extensions/quarto-journals/acm/add-bibliography.lua
+++ _extensions/quarto-journals/acm/add-bibliography.lua
@@ -1,4 +1,16 @@
 
+function has_class(el, class)
+  if el.attr == nil then
+    return nil
+  end
+  for i,v in ipairs(el.attr.classes) do
+    if v == class then
+      return true
+    end
+  end
+  return false
+end
+
 function get_block_with_class(el, class)
   return find_node(el, function(el)
     return has_class(el, class)
@@ -20,30 +32,57 @@ function attribute(el, name, default)
   return default
 end
 
+function replace_node(blocks, predicate, replacement)
+  for i, el in pairs(blocks) do
+    if predicate(el) then      
+      table.remove(blocks, i)
+      if type(replacement) == "function" then
+        replacement = replacement(el)
+      end
+      table.insert(blocks, i, replacement)
+      return el
+    end
+  end
+  return nil
+end
+
+local bibname
+
 return {
   {
+    Meta = function(meta)
+      bibname = pandoc.utils.stringify(meta.bibliography)
+    end,
     Pandoc = function(doc)
       if not quarto.doc.isFormat("latex") then
         return doc
       end
-      for i, el in pairs(doc.blocks) do
-        if el.t == "Header" and pandoc.utils.stringify(el.content) == "References" then
-          local bibname = "sample-base"
-          local bibliography = pandoc.RawBlock("latex", "\\bibliographystyle{ACM-Reference-Format}\n\\bibliography{" .. bibname .. "}")
-          -- latex inserts the new section anyway
-          table.remove(doc.blocks, i)
-          table.insert(doc.blocks, i, bibliography)
-          break
-        end
-      end
-      for i, el in pairs(doc.blocks) do
-        if el.t == "Header" and pandoc.utils.stringify(el.content) == "Appendix" then
-          local appendix = pandoc.RawBlock("latex", "\\appendix")
-          table.remove(doc.blocks, i)
-          table.insert(doc.blocks, i, appendix)
-          break
-        end
-      end
+
+      -- add bibliography to the right place
+      -- local bibname = "sample-base"
+      local bibliography = pandoc.RawBlock(
+        "latex", 
+        "\\bibliographystyle{ACM-Reference-Format}\n\\bibliography{" .. bibname .. "}")
+      replace_node(doc.blocks, function(el)
+        return el.t == "Header" and pandoc.utils.stringify(el.content) == "References"
+      end, bibliography)
+
+      -- fix appendix compilation
+      local appendix = pandoc.RawBlock("latex", "\\appendix")
+      replace_node(doc.blocks, function(el)
+        return el.t == "Header" and pandoc.utils.stringify(el.content) == "Appendix"
+      end, appendix)
+
+      -- fix acknowledgments
+      replace_node(doc.blocks, function(el)
+        return el.t == "Div" and has_class(el, "acks")
+      end, function(el)
+        local text = pandoc.write(pandoc.Pandoc(el.content), "latex")
+        local node = pandoc.RawBlock(
+          "latex", "\\begin{acks}\n" .. text .. "\\end{acks}")
+        return node        
+      end)
+
       return doc
     end
   }
